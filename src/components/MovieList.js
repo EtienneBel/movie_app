df3a9b8240ef4b5a072315ef7bafbc62d1ee14bc
Etienne Wend-Kuuni Belemgnegre <ebelemgnegre@oseogroup.com>
2024-06-14 19:04:38 +0000

    added: fetch on movie api

--- src/components/MovieList.js
+++ src/components/MovieList.js
@@ -3,6 +3,9 @@ import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import Slider from "react-slick";
 
+const API_KEY =
+  "eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI2NWE5YzJjMmFiZTMxNjYwMDMwOGZhMDJlMDY0NzVmNyIsInN1YiI6IjY1YmE3OTk1ZjkwYjE5MDE3YzA3MzU3ZiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.aU-C_y7WR97CP6-2TWZegkGVbBvXW_qPpD0FuNDuWoc";
+
 export default function MovieList({ list }) {
   var settings = {
     dots: true,
@@ -40,6 +43,10 @@ export default function MovieList({ list }) {
     ],
   };
 
+  const data = fetchMoviesListApi();
+  // data.map((item) => console.log(item));
+  // console.log(data);
+
   return (
     <div className="mt-12">
       {list.map((item, index) => (
@@ -56,4 +63,52 @@ export default function MovieList({ list }) {
       ))}
     </div>
   );
+}
+
+async function fetchMoviesGenreApi() {
+  const response = await fetch(
+    "https://api.themoviedb.org/3/genre/movie/list?language=en",
+    {
+      method: "GET",
+      headers: {
+        accept: "application/json",
+        Authorization: `Bearer ${API_KEY}`,
+      },
+    }
+  );
+
+  if (!response.ok) {
+    console.error("Error:", response.statusText);
+    return;
+  }
+
+  const data = await response.json();
+  // console.log(data);
+  return data;
+}
+
+async function fetchMoviesListApi() {
+  const data = await fetchMoviesGenreApi();
+  if (data) {
+    data.genres.forEach((genre) => {
+      // console.log(item);
+      fetch(
+        `https://api.themoviedb.org/3/discover/movie?api_key=${API_KEY}&with_genres=${genre.id}`,
+        {
+          method: "GET",
+          headers: {
+            accept: "application/json",
+            Authorization: `Bearer ${API_KEY}`,
+          },
+        }
+      )
+        .then((res) => res.json())
+        .then((data) => {
+          console.log(data);
+          return data;
+        });
+    });
+  } else {
+    console.log("No data available");
+  }
 }
